f7346377057c3eb5a98c760240d75bcaa5559394
Edwin9734 <100248651+Edwin9734@users.noreply.github.com>
2024-05-19 22:29:46 -0600

    Add files via upload

---
+++ MAIN/Main.java
@@ -0,0 +1,16 @@
+package org.example.MAIN;
+import org.example.evaluacion.Evaluators;
+
+import java.util.Scanner;
+
+import static org.example.evaluacion.Evaluators.evaluador;
+
+public class Main {
+    Evaluators a = new Evaluators();
+    public static void main(String[] args) {
+
+        evaluador();
+    }
+
+
+}
